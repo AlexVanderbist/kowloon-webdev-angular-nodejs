5f5ac43f90800ac77e1d66d50d2f94bf42ca8ee6
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-01-06 15:03:58 +0100

    finished image gallery for product

--- frontend/src/app/product/productController.js
+++ frontend/src/app/product/productController.js
@@ -3,6 +3,23 @@ angular
   .controller('ProductController', function () {
     var vm = this;
 
+    vm.activeImage = 0;
+
+    vm.images = [
+      {
+        url: "images/products/cooling_mat.jpg",
+        title: "woordje tekst"
+      },
+      {
+        url: "images/products/cooling_mat.jpg",
+        title: "woordje tekst"
+      },
+      {
+        url: "images/products/cooling_mat.jpg",
+        title: "woordje tekst"
+      }
+    ];
+
     function _init() {
     }
 
